0b5df8d46c6f370a68384d03a0ae2a30d03f359c
Bekhruz <bekhruz2012@gmail.com>
2023-11-10 22:54:10 +0500

    commit

--- src/Controller/blogController.php
+++ src/Controller/blogController.php
@@ -2,7 +2,31 @@
 
 namespace App\Controller;
 
-class blogController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class blogController extends AbstractController
 {
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return $this->render('homepage.html.twig', [
+        ]);
+    }
+    #[Route('/blog{error}')]
+    public function error1($error): Response
+    {
+        return $this->render('error.html.twig',[
+
+        ]);
+    }
+    #[Route('/blog')]
+    public function blogpage(): Response
+    {
+        return $this->render('blogpage.html.twig',[
+
+        ]);
+    }
 
 }
--- templates/login.php
+++ templates/login.php
@@ -1,1 +1,20 @@
 <?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve user input from the form
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Corrected user credentials
+    $validUsername = "user";
+    $validPassword = "user$$4321";
+
+    // Check if the entered username and password match the valid credentials
+    if ($username == $validUsername && $password == $validPassword) {
+        echo "Login successful!";
+    } else {
+        echo "Invalid username or password.";
+    }
+}
+
+?>
